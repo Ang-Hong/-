c504a0c7ede9ac1287277d2c31d2f9b3554140e1
Ang-Hong <70941880+Ang-Hong@users.noreply.github.com>
2022-10-29 16:55:04 +0800

    Update Wps3.js

--- Wps3.js
+++ Wps3.js
@@ -8,7 +8,7 @@ WPS会员
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/account.wps.cn.+$ url script-response-body wps.js
+^http[s]?:\/\/account.wps.cn.+$ url script-response-body https://raw.githubusercontent.com/Ang-Hong/-/main/Wps3.js
 [mitm] 
 hostname = *.wps.*
 *******************************
